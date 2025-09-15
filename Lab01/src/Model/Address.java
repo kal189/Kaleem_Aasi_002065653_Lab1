4a9b37167b3bfe749a1e96e2a35ba835b23af6dc
kal189 <kaleem.a@northeastern.edu>
2025-09-14 20:32:35 -0400

    Lab1_completed

--- Lab01/src/Model/Address.java
+++ Lab01/src/Model/Address.java
@@ -8,6 +8,40 @@ package Model;
  *
  * @author aasikaleem
  */
-class Address {
+public class Address {
+    String streetName;
+    String unitNum;
+    String city;
+    String Zipcode;
+
+    // Setting the getters and setters
+    public String getStreetName() {
+        return streetName;
+    }
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+    public String getUnitNum() {
+        return unitNum;
+    }
+    public void setUnitNum(String unitNum) {
+        this.unitNum = unitNum;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getZipcode() {
+        return Zipcode;
+    }
+    public void setZipcode(String zipcode) {
+        Zipcode = zipcode;
+    }
+
     
+
 }
+    
+
--- Lab01/src/Model/Product.java
+++ Lab01/src/Model/Product.java
@@ -10,4 +10,56 @@ package Model;
  */
 public class Product {
     
+    
+    
+    String name;
+    String description;
+    String availNum;
+    Address manufacturingAdd;
+    Address shippingAddress; 
+    int price;
+    
+    public Product() {
+    manufacturingAdd = new Address();
+    shippingAddress = new Address();
+}
+
+      public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String discription) {
+        this.description = discription;
+    }
+    public String getAvailNum() {
+        return availNum;
+    }
+    public void setAvailNum(String availNum) {
+        this.availNum = availNum;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public Address getManufacturerAddress() {
+        return manufacturingAdd;
+    }
+    public void setManufacturerAddress(Address manufacturerAddress) {
+        this.manufacturingAdd = manufacturerAddress;
+    }
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+
+    
 }
